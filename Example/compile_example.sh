7e4084e6764f71a9f3771604775b0754f98b8976
gmbo <gg.mbrb@gmail.com>
2020-05-22 13:42:24 -0300

    Renamed the Examples
    Now that I think about it, 'shellcode' is not quite a appropriate name.

--- Example/compile_example.sh
+++ Example/compile_example.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/bash
 
 progname="vulnerable_program"
-shellcode="shellcode.bin"
+new_func="new_func.bin"
 
 if [[ ! -x /usr/bin/nasm ]]; then
-	echo "nasm assembler is required to build the shellcode"
+	echo "nasm assembler is required for building the example"
 	exit 1
 fi
 
 gcc example_vulnerable_program.c -o ${progname}
-nasm -f elf64 example_shellcode.asm
+nasm -f elf64 example_new_func.asm
 
-text_offset="0x$(readelf -S example_shellcode.o | grep '\B\.text' | awk '{print $6}')"
-text_len="0x$(readelf -S example_shellcode.o | grep '\B\.text' -A1 | grep X | awk '{print $1}')"
+text_offset="0x$(readelf -S example_new_func.o | grep '\B\.text' | awk '{print $6}')"
+text_len="0x$(readelf -S example_new_func.o | grep '\B\.text' -A1 | grep X | awk '{print $1}')"
 
-xxd -s ${text_offset} -l ${text_len} -o -${text_offset} example_shellcode.o | xxd -r > ${shellcode}
+xxd -s ${text_offset} -l ${text_len} -o -${text_offset} example_new_func.o | xxd -r > ${new_func}
 
 echo "Vulnerable program: ${progname}"
-echo "Shellcode: ${shellcode}"
+echo "new_func: ${new_func}"
